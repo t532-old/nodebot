5fee2c2337ccd0c1128462379f5ecfdd4f5f5300
t532 <2037246484@qq.com>
2018-05-01 11:48:19 +0800

    start working on drawStat()

--- dist/modules/osubot/canvas.js
+++ dist/modules/osubot/canvas.js
@@ -53,7 +53,7 @@ var drawRecent = function () {
 
                     case 13:
                         _context.next = 15;
-                        return promisifyGM((0, _gm2.default)(dest).quality(100).composite('assets/image/' + rec.rank + '.png').gravity('North').geometry('+0+90'));
+                        return promisifyGM((0, _gm2.default)(dest).quality(100).composite('assets/image/rank/' + rec.rank + '.png').gravity('North').geometry('+0+90'));
 
                     case 15:
                         console.log('file://' + process.cwd() + _path2.default.sep + dest);
@@ -69,6 +69,52 @@ var drawRecent = function () {
 
     return function drawRecent(_x, _x2, _x3) {
         return _ref.apply(this, arguments);
+    };
+}();
+
+var drawStat = function () {
+    var _ref2 = (0, _asyncToGenerator3.default)( /*#__PURE__*/_regenerator2.default.mark(function _callee2(stat) {
+        var uid, dest;
+        return _regenerator2.default.wrap(function _callee2$(_context2) {
+            while (1) {
+                switch (_context2.prev = _context2.next) {
+                    case 0:
+                        uid = stat.user_id;
+                        dest = 'cache' + _path2.default.sep + 'osubot' + _path2.default.sep + 'stat' + _path2.default.sep + uid + '.jpg';
+                        _context2.next = 4;
+                        return _web.res.avatarQuery(uid, 'cache' + _path2.default.sep + 'osubot' + _path2.default.sep + 'avatar' + _path2.default.sep + uid + '.jpg');
+
+                    case 4:
+                        _fs2.default.copyFileSync('assets' + _path2.default.sep + 'osubot' + _path2.default.sep + 'assets' + _path2.default.sep + 'image' + _path2.default.sep + 'userbg' + _path2.default.sep + 'c' + Math.floor(Math.random() * 6) + '.jpg', dest);
+                        _context2.next = 7;
+                        return promisifyGM((0, _gm2.default)(dest).quality(100).resize(2765, 768));
+
+                    case 7:
+                        _context2.next = 9;
+                        return promisifyGM((0, _gm2.default)(dest).quality(100).gravity('Center').crop(1500, 500).fill('#888b').drawCircle(750, 250, 750, 620).tile(dest).drawCircle(750, 250, 750, 610));
+
+                    case 9:
+                        _context2.next = 11;
+                        return promisifyGM((0, _gm2.default)(dest).quality(100).gravity('Center').fill('#fffa').drawCircle(750, 250, 750, 610).fill('#fff').drawCircle(750, 250, 750, 490).fill('#ccc').drawCircle(750, 250, 750, 470).fill('#fff').drawCircle(750, 250, 750, 460).fill('#aaa').drawEllipse(750, 250, 210, 210, -145, -35).fill('#fff').font('assets/fonts/Exo2.0-Regular.otf').fontSize(30).drawText(0, -145, stat.username).font('assets/fonts/Exo2.0-Bold.otf').fontSize(12).fill('#f69').drawText(0, 0, 'global rank').font('assets/fonts/Exo2.0-BoldItalic.otf').fontSize(25).fill('#3ad').drawText(0, 35, map.title.slice(0, 35) + (map.title.length > 35 ? '...' : '')).fontSize(17).drawText(0, 60, map.artist.slice(0, 50) + (map.artist.length > 50 ? '...' : '')).font('assets/fonts/Exo2.0-Bold.otf').fontSize(30).drawText(-300, 0, rec.maxcombo + 'x').drawText(300, 0, stat.accuracy.slice(0, 3 + stat.accuracy.split('.').length) + '%').fontSize(12).fill('#333').drawText(-290, 20, 'max combo').drawText(290, 20, 'accuracy').font('assets/fonts/Venera-500.otf').fontSize(50).fill('#f69').drawText(0, -25, _util2.default.scorify(stat.pp_rank)).font('assets/fonts/Exo2.0-Bold.otf').fontSize(13).fill('#999').drawText(0, 85, map.version + ' - mapped by ' + map.creator).drawRectangle(675, 345, 825, 365).font('assets/fonts/Exo2.0-Regular.otf').fill('#fff').drawText(0, 105, rec.date).fontSize(25).fill('#aaa').drawLine(650, 375, 850, 375).fill('#666').drawText(-100, 140, _util2.default.fillNumber(rec.count300)).drawText(-33, 140, _util2.default.fillNumber(rec.count100)).drawText(33, 140, _util2.default.fillNumber(rec.count50)).drawText(100, 140, _util2.default.fillNumber(rec.countmiss)).fontSize(12).drawText(-100, 160, 'Great').drawText(-33, 160, 'Good').drawText(33, 160, 'Meh').drawText(100, 160, 'Miss').crop(1200, 500));
+
+                    case 11:
+                        _context2.next = 13;
+                        return promisifyGM((0, _gm2.default)(dest).quality(100).composite('assets/image/' + rec.rank + '.png').gravity('North').geometry('+0+90'));
+
+                    case 13:
+                        console.log('file://' + process.cwd() + _path2.default.sep + dest);
+                        return _context2.abrupt('return', 'file://' + process.cwd() + _path2.default.sep + dest);
+
+                    case 15:
+                    case 'end':
+                        return _context2.stop();
+                }
+            }
+        }, _callee2, this);
+    }));
+
+    return function drawStat(_x4) {
+        return _ref2.apply(this, arguments);
     };
 }();
 
