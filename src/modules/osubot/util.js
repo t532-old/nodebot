f0708be37582dca99c9094f115d3720d2ad4bb11
t532 <2037246484@qq.com>
2018-05-18 00:54:06 +0800

    fix bugs

--- src/modules/osubot/util.js
+++ src/modules/osubot/util.js
@@ -53,7 +53,7 @@ export default {
      * @param {object} diffee - The substractors
      */
     objDiff(differ, diffee) {
-        const result = differ
+        const result = this.copy(differ)
         for (i in Object.keys(differ))
             if (typeof differ[i] === 'number' && typeof diffee[i] === 'number')
                 result[i] = differ[i] - diffee[i]
@@ -81,7 +81,7 @@ export default {
     flatten(arr) {
         const flat = []
         for (let i of arr) {
-            if (i instanceof Array) flat.push(...this.flatten(i))
+            if (i instanceof Array) flat = [...flat, ...this.flatten(i)]
             else flat.push(i)
         }
         return flat
