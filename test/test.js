56696f89703b8779277440d642ec6c69a75cfa40
t532 <2037246484@qq.com>
2018-05-18 01:11:34 +0800

    better tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,12 @@
 // I didn't use ES6/7 Syntax here because babel seems wouldn't support importing after some other statements.
+console.log(
+`-------------------------------
+Running runtime tests...`
+)
 require('fs').copyFileSync('config.template.yml', 'config.yml')
-require('../dist/message')
+console.log('- 1/3: Load config')
 require('../dist/modules/init')
+console.log('- 2/3: Load Initialization script')
+require('../dist/message')
+console.log('- 3/3: Load main bot')
+console.log(`- ✔ Success`)
