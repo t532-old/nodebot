ae46d7adf156b21fa19115f00cda15b53758004e
t532 <2037246484@qq.com>
2018-07-05 18:26:31 +0800

    add repeater config (2/2, #35)

--- src/modules/osubot/middleware.js
+++ src/modules/osubot/middleware.js
@@ -1,18 +1,24 @@
+import yaml from 'js-yaml'
+import { readFileSync } from 'fs'
 const log = {
     private: {},
     group: {},
 }
+const { repeater: config } = yaml.safeLoad(readFileSync('config.yml')).osubot
 /**
  * This middleware counts the repeated times of a message
  * if it reaches 3, the bot repeats it
  * @param {Message} msg 
  */
 export default function repeater(msg) {
-    if (/!|！/.test(msg.param.message) === false) {
+    if (/!|！/.test(msg.param.message) === false && 
+        msg.type === 'group') {
+        if (notAllowed in config && config.notAllowed.includes(msg.target)) return
+        if (allowed in config && !config.allowed.includes(msg.target)) return
         if (!log[msg.type][msg.target]) log[msg.type][msg.target] = { count: 1, message: msg.param.message }
         else if (msg.param.message === log[msg.type][msg.target].message) log[msg.type][msg.target].count++
         else log[msg.type][msg.target] = { count: 1, message: msg.param.message }
-        if (log[msg.type][msg.target].count === 3) {
+        if (log[msg.type][msg.target].count === config.times) {
             const timeout = Math.round(Math.random() * 200000),
                   repeatTarget = log[msg.type][msg.target]
             console.log(`[MOD] ${new Date().toString()} by osubot middleware: attempting to repeat \`${msg.param.message}' in ${msg.type} ${msg.target} in ${Math.round(timeout / 1000)} secs`)
