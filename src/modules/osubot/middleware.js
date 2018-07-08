396e7f6a3d51727c8c75c121b71c1ab381495a89
t532 <2037246484@qq.com>
2018-07-09 07:31:05 +0800

    fix repeater bug

--- src/modules/osubot/middleware.js
+++ src/modules/osubot/middleware.js
@@ -22,7 +22,7 @@ export default function repeater(msg) {
         if (log[msg.type][msg.target].count === config.times) {
             const timeout = Math.round(Math.random() * 200000),
                   repeatTarget = log[msg.type][msg.target]
-            console.log(`${chalk.blue('[MOD]')} ${chalk.gray(new Date().toString())} by osubot middleware: attempting to repeat \`${msg.param.message}' in ${this.type === 'group' ? `${this.type} ${this.target}` : chalk.yellow(`${this.type} ${this.target}`)} in ${Math.round(timeout / 1000)} secs`)
+            console.log(`${chalk.blue('[MOD]')} ${chalk.gray(new Date().toString())} by osubot middleware: attempting to repeat \`${msg.param.message}' in ${msg.type === 'group' ? `${msg.type} ${msg.target}` : chalk.yellow(`${msg.type} ${msg.target}`)} in ${Math.round(timeout / 1000)} secs`)
             setTimeout(() => { msg.send(repeatTarget.message) }, timeout)
             delete log[msg.type][msg.target]
         } 
