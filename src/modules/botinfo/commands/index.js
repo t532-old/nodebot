d21e1876b4699e5beef30aa15445073abc9f2c40
t532 <2037246484@qq.com>
2018-07-28 19:35:44 +0800

    finish the helper module, bump version

--- src/modules/botinfo/commands/index.js
+++ src/modules/botinfo/commands/index.js
@@ -1,3 +1,2 @@
 export about from './about'
-export help from './help'
 export usage from './usage'
--- src/modules/helper/canvas.js
+++ src/modules/helper/canvas.js
@@ -1,5 +1,6 @@
-const gm = require('gm')
-const { copyFileSync } = require('fs')
+import { subClass } from 'gm'
+import { copyFileSync } from 'fs'
+const gm = subClass({ imageMagick: true })
 function promisifyGM(gmO) {
     return new Promise(function(resolve, reject) {
         gmO.write(gmO.source, err => {
@@ -9,14 +10,57 @@ function promisifyGM(gmO) {
     })
 }
 const cachepath = 'cache/helper'
-export default async function drawHelp(help) {
-    const dest = `${cachepath}/${help.name}.jpg`
+export async function drawHelp(name, help) {
+    const dest = `${cachepath}/${name}.jpg`
+    const description = []
+    copyFileSync(`assets/helper/image/template.jpg`, dest)
+    help.description = help.description.split('')
+    while (help.description.length) description.push(help.description.splice(0, 20).join(''))
+    await promisifyGM(
+        gm(dest)
+        .quality(100)
+        .font('assets/helper/fonts/Microsoft-YaHei.ttc')
+        .fill('#777')
+        .fontSize(50)
+        .drawText(0, 50, help.name)
+        .fill('#aaa')
+        .fontSize(30)
+        .drawText(20, 70, '用途')
+        .drawText(370, 70, '参数')
+        .drawText(370, 250, '选项')
+        .fill('#fff')
+        .fontSize(15)
+        .drawText(20, 110, description.join('\n'))
+        .drawText(370, 100, help.parameters.join('\n\n'))
+        .drawText(370, 300, help.options.join('\n\n'))
+        .fontSize(15)
+        .fill('#aaa')
+        .gravity('SouthEast')
+        .drawText(20, 20, `来自模块 ${help.module}`)
+    )
+}
+export async function drawHelpList(list) {
+    const dest = `${cachepath}/_main.jpg`
     copyFileSync(`assets/helper/image/template.jpg`, dest)
     await promisifyGM(
         gm(dest)
         .quality(100)
         .font('assets/helper/fonts/Microsoft-YaHei.ttc')
+        .fill('#777')
+        .fontSize(50)
+        .drawText(0, 50, '帮助中心')
+        .fill('#aaa')
+        .fontSize(30)
+        .drawText(20, 70, '可用的帮助列表：')
         .fill('#fff')
-        .drawText(100, 100, 'Test')
+        .fontSize(20)
+        .drawText(20, 110, list.slice(0, 10).map(i => `· ${i}`).join('\n'))
+        .drawText(220, 110, list.slice(10, 20).map(i => `· ${i}`).join('\n'))
+        .drawText(420, 110, list.slice(20, 30).map(i => `· ${i}`).join('\n'))
+        .drawText(620, 110, list.slice(30, 40).map(i => `· ${i}`).join('\n'))
+        .fontSize(20)
+        .fill('#aaa')
+        .gravity('SouthEast')
+        .drawText(20, 20, '使用 -help <命令名称> 来查询帮助。')
     )
 }
