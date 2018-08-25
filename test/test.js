c2ffe034d90c64506156ec513b6c4f1599ff0b2a
t532 <2037246484@qq.com>
2018-08-25 11:48:13 +0800

    modify test build script

--- test/test.js
+++ test/test.js
@@ -4,9 +4,9 @@ console.log(
 Running runtime tests...`
 )
 console.log('- 1/2: Load config:')
-require('fs').copyFileSync('config.template.yml', 'config.yml')
+require('fs').copyFileSync('config/config.template.yml', 'config/config.yml')
 console.log('  - config.yml')
-require('fs').copyFileSync('aliases.template.yml', 'aliases.yml')
+require('fs').copyFileSync('config/aliases.template.yml', 'config/aliases.yml')
 console.log('  - aliases.yml')
 console.log('- 2/2: Load Core:')
 console.log('  - 1/3: Load Command Parser:')
